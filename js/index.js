4f01878810beb1b2252e9dbcedb2d459eb57eea3
Laxman Pal <laxi27902@gmail.com>
2023-04-16 11:36:40 +0530

    added custom cursor and footer

--- js/index.js
+++ js/index.js
@@ -25,3 +25,38 @@ const mobileMenu = document.querySelector(".mobile-menu");
 mobileMenuContainer.addEventListener("click", (e) => {
   mobileMenu.classList.toggle("active");
 });
+
+// Cursor
+
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  innerCursor.style.left = `${x}px`;
+  innerCursor.style.top = `${y}px`;
+  setTimeout(() => {
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+  }, 100);
+}
+
+let links = Array.from(
+  document.querySelectorAll(
+    "li , .logo , .mobile-menu , .social-link , .copyright"
+  )
+);
+
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    innerCursor.classList.add("grow");
+    outerCursor.style.display = "none";
+  });
+  link.addEventListener("mouseleave", () => {
+    innerCursor.classList.remove("grow");
+    outerCursor.style.display = "block";
+  });
+});
